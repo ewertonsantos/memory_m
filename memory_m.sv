9aa4eec7dff8b5241a2993301598fc1ac4d828fc
Ewerton Santos <ewertonredsnop@gmail.com>
2023-03-19 22:02:23 -0300

    turn high impedancy dinamic

--- memory_m.sv
+++ memory_m.sv
@@ -1,3 +1,4 @@
+// Code your design here
 module memory_m #(parameter DWIDTH = 8, AWIDTH = 5) (
   inout logic [DWIDTH-1:0] data,
   input logic [AWIDTH-1:0] addr,
@@ -5,7 +6,7 @@ module memory_m #(parameter DWIDTH = 8, AWIDTH = 5) (
   input logic write
 );
   // internal registar thats isnt input or output, its the reason its here
-  reg [DWIDTH-1:0] reg_array [2**AWIDTH-1:0];
+  reg [DWIDTH-1:0] reg_array [(2**AWIDTH)-1:0];
   // intermadiate variable for data when its treated as output 
   //cause we cannot pass a register directly to a logical inside awalys block
   logic [7:0] temp_data;
@@ -14,7 +15,7 @@ module memory_m #(parameter DWIDTH = 8, AWIDTH = 5) (
     if (read & ~write)
       temp_data = reg_array[addr];
     else
-      temp_data = 8'bzzzzzzzz;
+      temp_data = {DWIDTH{1'bz}};;
   end 
     
   always_ff @(posedge write) begin
